6bb422695688d3ee06b253d375234830bb677b81
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-19 03:55:53 -0400

    Example dotenv

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
 
+require('dotenv').config();
+
 const PORT = 8000;
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
 const app = express();
 
